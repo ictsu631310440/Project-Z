d0d75093568675368ddd298d1495da7702638a81
ictsu631310440 <99553613+ictsu631310440@users.noreply.github.com>
2022-11-16 01:05:31 +0700

    Update Ui

--- BaconRun/Assets/Scripts/old/oldSceneController.cs
+++ BaconRun/Assets/Scripts/old/oldSceneController.cs
@@ -4,45 +4,62 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class SceneController : MonoBehaviour
+public class oldSceneController : MonoBehaviour
 {
-    public GameObject txt_Time;
+    public GameObject txtTime;
+    public GameObject MainMenu;
     public GameObject PauseMenu;
     public GameObject GameOverUI;
     private float time = 0.0f;
     
     public static bool OpenGameoverUI = false;
     public static bool OpenwinnerUI = false;
-
+    public static bool Startgame = false;
+    
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 0;
+        txtTime.SetActive(true);
+        Startgame = false;
+        
+        
         OpenGameoverUI = false;
         OpenwinnerUI = false;
         
-        PauseMenu.SetActive(true);
+        PauseMenu.SetActive(false);
+        MainMenu.SetActive(true);
     }
 
     public void ResumeGame()
     {
-        txt_Time.SetActive(true);
         PauseMenu.SetActive(false); //ปิดหน้าต่างหยุด
+        MainMenu.SetActive(false);
+        
         Time.timeScale = 1; //เวลาในเกมเดินต่อ
+        txtTime.SetActive(true);
+        Startgame = true;
+        
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         time = time + Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 1 && OpenGameoverUI != true) 
+
+        if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 1 && OpenGameoverUI != true && Startgame == true)
         {
+            txtTime.SetActive(false);
+            MainMenu.SetActive(false);
             PauseMenu.SetActive(true);
             Time.timeScale = 0;
+            
             Cursor.visible = true;
         }
-        else if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 0 && OpenGameoverUI!=true)
+        else if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 0 && OpenGameoverUI != true &&
+                 Startgame == true)
         {
             ResumeGame();
         }
